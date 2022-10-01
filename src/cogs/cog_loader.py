8b85969de53a33dedee993e04ace4815f8ca9900
ldprice <liam@ldprice.com>
2022-10-01 15:13:38 +1000

    patched cog_loader.py

--- src/cogs/cog_loader.py
+++ src/cogs/cog_loader.py
@@ -165,43 +165,38 @@ class CogLoader(commands.Cog):
                 color=0x808080
             )
             ext = f"{cog.lower()}.py"
-            if not os.path.exists(f"./cogs/{ext}"):
-                # if the file does not exist
-                embed.add_field(
-                    name=f"Failed to reload: `{ext}`",
-                    value="This cog does not exist.",
-                    inline=False
-                )
 
-            elif ext.endswith(".py") and not ext.startswith("_"):
+            try:
                 # upgrade this cog by downloading from GitHub:
                 r = requests.get(f'https://raw.githubusercontent.com/LeehamElectronics/SkyNet-Discord-Bot/master/src/cogs/{ext}')
                 f = open(f"cogs/{ext}", 'wb')
                 f.write(r.content)
+            except Exception as error:
+                await interaction.response.send_message('failed to download cog!', ephemeral=True)
 
-                try:
-                    await self.bot.unload_extension(f"cogs.{ext[:-3]}")
+            try:
+                await self.bot.unload_extension(f"cogs.{ext[:-3]}")
+                await self.bot.load_extension(f"cogs.{ext[:-3]}")
+                embed.add_field(
+                    name=f"Reloaded: `{ext}`",
+                    value='\uFEFF',
+                    inline=False
+                )
+            except Exception:
+                try:  # Run a second try method in case the cog was already unloaded.
                     await self.bot.load_extension(f"cogs.{ext[:-3]}")
                     embed.add_field(
-                        name=f"Reloaded: `{ext}`",
+                        name=f"Loaded: `{ext}`",
                         value='\uFEFF',
                         inline=False
                     )
-                except Exception:
-                    try:  # Run a second try method in case the cog was already unloaded.
-                        await self.bot.load_extension(f"cogs.{ext[:-3]}")
-                        embed.add_field(
-                            name=f"Loaded: `{ext}`",
-                            value='\uFEFF',
-                            inline=False
-                        )
-                    except Exception as e:
-                        # desired_trace = traceback.format_exc()
-                        embed.add_field(
-                            name=f"Failed to reload: `{ext}`",
-                            value=e,
-                            inline=False
-                        )
+                except Exception as e:
+                    # desired_trace = traceback.format_exc()
+                    embed.add_field(
+                        name=f"Failed to reload: `{ext}`",
+                        value=e,
+                        inline=False
+                    )
             await interaction.response.send_message(embed=embed, ephemeral=True)
 
     @upgrade_cogs.autocomplete('cog')
