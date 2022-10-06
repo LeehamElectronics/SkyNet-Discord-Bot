a7705f5cae7cee9148e167bdefedddfc6f456085
ldprice <liam@ldprice.com>
2022-10-06 13:40:37 +1100

    more comprehensive verify command

--- src/cogs/admin_commands.py
+++ src/cogs/admin_commands.py
@@ -1,3 +1,5 @@
+import typing
+
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -22,21 +24,40 @@ class AdminCommands(commands.Cog):
 
     @app_commands.checks.has_any_role('Owner', 'Admin')
     @app_commands.command(name="verify", description="verify user into server")
-    async def verify_user_command(self, interaction: discord.Interaction, verify_type: str, user: discord.Member) -> None:
-        if not user:
-            await interaction.response.send_message('What User?', ephemeral=True)
-        else:
+    async def verify_user_command(self, interaction: discord.Interaction, user: typing.Optional[discord.Member], mc_username: typing.Optional[str] = None) -> None:
+        if not user and not mc_username:
+            await interaction.response.send_message('Please specify a discord or MC username or both', ephemeral=True)
+
+        elif mc_username is None:
+            # only verify Discord member
             member_role = get(interaction.guild.roles, id=543039798668034048)
             if member_role in user.roles:
-                await interaction.response.send_message(f'This user is already verified!', ephemeral=True)
+                await interaction.response.send_message(f'{user.mention} is already verified!', ephemeral=True)
             else:
-                await interaction.response.send_message(f'Verifying Member on {verify_type}...', ephemeral=True)
+                await interaction.response.send_message(f'Verifying {user.mention} on Discord...', ephemeral=True)
                 await user.add_roles(member_role)
+        elif user is None:
+            # only verify MC username
+            await interaction.response.send_message(f'Verifying {mc_username} on the MC server...', ephemeral=True)
+        else:
+            # verify both discord and MC accounts and link them together!
+            await interaction.response.send_message(f'Verifying {user.mention} on Discord and {mc_username} on MC', ephemeral=True)
+
+    @verify_user_command.autocomplete('service')
+    async def verify_autocomplete(self, interaction: discord.Interaction, current: str
+                                       ) -> list[app_commands.Choice[str]]:
+        list_of_services = ['discord', 'mc']
+
+        return [
+            app_commands.Choice(name=service, value=service)
+            for service in list_of_services if current.lower() in service.lower()
+        ]
 
     # error handler
     async def on_app_command_error(self, interaction: Interaction, error: AppCommandError):
         embed = diagnostics.log_error('severe', 'command', 'Command failed to run', str(error), 'admin_commands.py')
         await self.error_log_channel.send(embed=embed)
+        await interaction.response.send_message(f'Oops the command failed!', ephemeral=True)
 
 
 async def setup(bot: commands.Bot) -> None:
